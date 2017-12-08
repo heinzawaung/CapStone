03ba639194c59d08c74a8fea14cd78f757755d94
Hein Zaw Aung <allking99@gmail.com>
2017-12-08 09:25:25 +0630

    change

--- MCommerce/Controller/ProductDetailViewController.swift
+++ MCommerce/Controller/ProductDetailViewController.swift
@@ -47,11 +47,7 @@ class ProductDetailViewController: UIViewController {
       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
-        // Dispose of any resources that can be recreated.
-    }
+    
     
     @IBAction func decreaseQuantity(_ sender: Any) {
         if self.quantity <= 1{
